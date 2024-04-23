781fa455f3ea574fccd452432c80c7a819b186ca
Vivian Ly <viv.ly.2803@gmail.com>
2024-04-22 23:23:11 -0500

    Final Solutions

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:id>/', views.product_detail,
+name='product_detail'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Product
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/list.html', {'products': products})
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'products/detail.html', {'product': product})
